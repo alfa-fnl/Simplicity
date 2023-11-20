7d1d87721dff8b02d49f9b1fdec2b19b77b2ade8
alfa-fnl <117150748+alfa-fnl@users.noreply.github.com>
2023-11-20 21:07:30 +0700

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,7 @@ using System.IO;
 using System.Globalization;
 using CsvHelper.Configuration;
 
-namespace ModularityTestingApp
+namespace Simplicity
 {
     public partial class Form1 : Form
     {
